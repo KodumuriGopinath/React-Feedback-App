956a81fa6e164712792db5fa6aa39ad046b8dbe7
gopinath <gopinath@exaple.com>
2023-03-19 06:11:49 +0000

    Feedback App Solution

--- src/components/Feedback/index.js
+++ src/components/Feedback/index.js
@@ -4,30 +4,57 @@ import {Component} from 'react'
 import './index.css'
 
 class Feedback extends Component {
+  state = {isClicked: true}
+
+  onEmojiButtonClicked = () => {
+    this.setState({isClicked: false})
+  }
+
   render() {
     const {resources} = this.props
+    const {isClicked} = this.state
+    console.log(isClicked)
 
     return (
       <div className="bg-container">
         <div className="card-container">
-          <h1>How satisfied are you with our customer support performance?</h1>
-          <div>
-            <ul className="total-list-container">
-              {resources.emojis.map(eacthItem => (
-                <li className="list-item" key={eacthItem.id}>
-                  <div className="emoji-and-name">
-                    <img
-                      className="emoji-image"
-                      src={eacthItem.imageUrl}
-                      alt=""
-                      onClick="display('sectionTwo')"
-                    />
-                    <p>{eacthItem.name}</p>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
+          {isClicked ? (
+            <>
+              <h1 className="heading">
+                How satisfied are you with our customer support performance?
+              </h1>
+              <div>
+                <ul className="total-list-container">
+                  {resources.emojis.map(eacthItem => (
+                    <li className="list-item" key={eacthItem.id}>
+                      <div className="emoji-and-name">
+                        <img
+                          className="emoji-image"
+                          src={eacthItem.imageUrl}
+                          alt={eacthItem.name}
+                          onClick={this.onEmojiButtonClicked}
+                        />
+                        <p>{eacthItem.name}</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </>
+          ) : (
+            <div className="second-card">
+              <img
+                className="love-image"
+                src={resources.loveEmojiUrl}
+                alt="love emoji"
+              />
+              <h2 className="love-heading">Thank You!</h2>
+              <p className="description">
+                We will use your feedback to improve our customer support
+                performance
+              </p>
+            </div>
+          )}
         </div>
       </div>
     )
